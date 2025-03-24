4840a7216973a20de52709ab129f827c149df102
lnettf <luisrnett@gmail.com>
2025-03-24 19:33:49 +0100

    delete contact messagge

--- src/pages/contactEdit/ContactEdit.jsx
+++ src/pages/contactEdit/ContactEdit.jsx
@@ -1,15 +1,30 @@
 import axios from "axios";
+import { Fragment } from "react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 export const ContactEdit = () => {
   const [contact, setContact] = useState(null);
   const { contactId } = useParams();
+  let navigate = useNavigate();
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:3000/contact/${contactId}`);
+      alert("Se ha borrado correctamente");
+    } catch (error) {
+      console.error({ error });
+    }
+    navigate("/contact-list");
+  };
 
   useEffect(() => {
     const get = async () => {
       try {
-        await axios.get(`http://localhost:3000/contact/${contactId}`);
+        const response = await axios.get(
+          `http://localhost:3000/contact/${contactId}`
+        );
+        setContact(response.data.data);
       } catch (error) {
         console.log({ error });
       }
@@ -21,5 +36,12 @@ export const ContactEdit = () => {
     return <h1>no existe el contacto</h1>;
   }
 
-  return <h1>ConctactEdit - {contactId}</h1>;
+  return (
+    <>
+      <h1>Nombre: {contact.name}</h1>
+      <h2>Apellidos: {contact.surname}</h2>
+      <pre>{JSON.stringify(contact)}</pre>
+      <button onClick={handleDelete}>Borrar</button>
+    </>
+  );
 };
